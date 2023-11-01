6e367708996e305238b45e5203bae4cb10eb84a8
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-11-01 16:48:02 -0300

    update

--- mais-typescript/tuples/exercise.js
+++ mais-typescript/tuples/exercise.js
@@ -5,3 +5,27 @@
   2. Defina o tipo/interface de cada venda (tuple).
   3. Some o total das vendas e mostre na tela.
 */
+// Função chamada fetchVendas responsável por fazer uma requisição para a API e retornar um objeto. O async está tornando a função assíncrona, ou seja, ela vai esperar os await serem resolvidos para depois retornar o valor.
+async function fetchVendas() {
+    // O await está falando para esperar as respostas das requisições para depois armazenar o retorno na variável.
+    const response = await fetch("https://api.origamid.dev/json/vendas.json"); // Faz uma requisição para a url da api e armazena o retorno(a resposta) na variável response.
+    const data = await response.json(); // Converte o retorno da requisição para json e armazena na variável data.
+    return somarVendas(data); // Retorna a função somarVendas passando o valor da variável data como parâmetro.
+}
+fetchVendas();
+function somarVendas(vendas) {
+    //* > Utilizando o for:
+    let total1 = 0; // Criado uma variável chamada total1 do tipo number e atribuido o valor 0.
+    // O for recebe um inciciador com o valor 0, uma condição que verifica se o valor do inciciador é menor que o tamanho do array vendas e se for executa o for, e um incrementador que incrementa o valor do inciciador em 1.
+    for (let i = 0; i < vendas.length; i++) {
+        total1 += vendas[i][1]; // Acessa o array vendas na posição i(array atual) e na posição 1(posição do preço) e soma com o valor da variável total1.
+    }
+    document.body.innerHTML = `<h1>Total de vendas: R$ ${total1}</h1>`; // Exibe no body o valor da variável total1 dentro de uma tag h1.
+    //* > Utilizando o reduce:
+    // Está sendo utilizado o reduce para somar os valores do array vendas, o reduce recebe uma função que recebe dois parâmetros, o primeiro é o valor anterior e o segundo é o valor atual. Começa com o valor 0.
+    const total2 = vendas.reduce((anterior, atual) => {
+        return anterior + atual[1]; // Retorna o valor anterior somado com o valor atual na posição 1(posição do preço).
+    }, 0);
+    console.log(total2);
+    document.body.innerHTML = `<h1>Total de vendas: R$ ${total2}</h1>`; // Exibe no body o valor da variável total2 dentro de uma tag h1.
+}
--- mais-typescript/tuples/script.js
+++ mais-typescript/tuples/script.js
@@ -11,7 +11,29 @@ if (typeof produto1[0] === "string") {
 }
 // Utilizando o tuples:
 const produto2 = ["Notebook", 2000]; // Criado uma array chama produto2 que possui duas propriedades, já definindo o tipo de cada propriedade.
+console.log(produto2[0].toLowerCase()); // Acessa o primeiro elemento do array e converte a string para lowercase e exibe no console.
+console.log(produto2[1].toFixed()); // Acessa o segundo elemento do array e converte o number para string e exibe no console.
+const [nome, preco] = produto2; // Desestrutura o array produto2 e atribui os valores para as variáveis nome e preco. Sendo que ele armazena seguindo a ordem do array.
+console.log(nome, preco); // Exibe no console o valor da variável nome e preco.
 //* > as const.
 /*
   - Torna um dado readonly e infere o tipo de dado mais específico possível. Em métodos que retornam Array's, as mesmas são transformadas em Tuples.
 */
+// Criado uma função chamada getText que recebe um parâmetro do tipo string e retorna um array com um elemento do tipo HTMLElement e outro do tipo string, ou null se o elemento não existir.
+function getText(selector) {
+    const element = document.querySelector(selector); // Seleciona o elemento do DOM passado como parâmetro e armazena na variável element como HTMLElement.
+    // Se o elemento existir executa o if.
+    if (element) {
+        // O as const torna o retorno da função mais específico, ou seja, ele retorna um array com dois elementos, sendo o primeiro do tipo HTMLElement e o segundo do tipo string. O elemento não pode ser alterado, pois ele é readonly.
+        return [element, element.innerText]; // Retorna um array com o elemento e o texto dentro do elemento.
+    }
+    else {
+        return null; // Retorna null ou seja, não existe o elemento.
+    }
+}
+const button = getText("button"); // Armazena o retorno da função getText na variável button.
+console.log(button); // Exibe no console o valor da variável button.
+if (button) {
+    // O TypeScript sabe que o primeiro elemento do array é um HTMLElement, pois foi definido na função getText, então ele consegue acessar as propriedades do HTMLElement.
+    button[0].classList.add("new-class"); // Adiciona uma nova classe no elemento.
+}
